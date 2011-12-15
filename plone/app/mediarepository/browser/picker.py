91a9e59353b4df0534404e9eb0363650e8f5ce58
Jamie Lentin <jm@lentin.co.uk>
2011-12-15 16:07:11 +0000

    Ditch hardcoded path from picker also, use any image repository available instead

--- plone/app/mediarepository/browser/picker.py
+++ plone/app/mediarepository/browser/picker.py
@@ -1,3 +1,4 @@
+from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 
 
@@ -6,9 +7,14 @@ class Picker(BrowserView):
         self.context = context
         BrowserView.__init__(self, context, request)
 
-    def getUploadUrl(self):
-        folder_url = self.context['media-repository'].absolute_url()
-        return '%s/@@quick_upload' % folder_url
+    def getUploadUrl(self, repo):
+        return '%s/@@quick_upload' % repo.absolute_url_path()
 
-    def getDataForUploadUrl(self):
+    def getDataForUploadUrl(self, repo):
         return 'data_url'
+
+    def getRepositories(self):
+        """Fetch everything of type plone.mediarepository"""
+        catalog = getToolByName(self.context, 'portal_catalog')
+        results = [b.getObject() for b in catalog.searchResults(portal_type='plone.mediarepository')]
+        return results
