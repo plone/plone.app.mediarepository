0f51999deedfa1862c6209d8a43f8eb0b2c5ac82
Timo Stollenwerk <contact@timostollenwerk.net>
2012-04-20 08:13:31 +0200

    Make sure plone.mediarepository is linkable in TinyMCE. It is strange that this works only when excluding 'plone.mediarepository' and 'Media Repository' (the title of the dexterity type).

--- plone/app/mediarepository/tests/test_mediarepository.py
+++ plone/app/mediarepository/tests/test_mediarepository.py
@@ -1,13 +1,22 @@
+from zope.component import getUtility
 import unittest2 as unittest
 
+from Products.TinyMCE.interfaces.utility import ITinyMCE
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 
-from plone.app.mediarepository.testing import MEDIAREPOSITORY_INTEGRATION_TESTING
+from plone.app.mediarepository.testing import \
+    MEDIAREPOSITORY_INTEGRATION_TESTING
+
 
 class MediaRepositoryTest(unittest.TestCase):
 
     layer = MEDIAREPOSITORY_INTEGRATION_TESTING
+
+    def test_mediarepository_is_linkable_in_tinymce(self):
+        utility = getUtility(ITinyMCE)
+        linkable_portal_types = utility.linkable.split('\n')
+        self.assertTrue('plone.mediarepository' in linkable_portal_types)
 
     def test_create_repository(self):
         portal = self.layer['portal']
@@ -16,4 +25,5 @@ class MediaRepositoryTest(unittest.TestCase):
         portal.invokeFactory('plone.mediarepository', 'repo')
 
         self.assertTrue('repo' in portal)
-        self.assertEquals(portal['repo'].portal_type, 'plone.mediarepository')
+        self.assertEquals(portal['repo'].portal_type,
+            'plone.mediarepository')
